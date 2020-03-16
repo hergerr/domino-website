7c982a509752725f8838796d554dc6bdf890ee10
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2020-03-16 12:17:18 +0100

    Added animation timeout

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,17 @@
 var slideIndex = 1;
 showSlides(slideIndex);
+plusSlidesTime(slideIndex)
 
 // Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
+}
+
+function plusSlidesTime(n) {
+  myInterval = window.setInterval(function () {
+    // increment the step
+    showSlides(slideIndex += n);
+  }, 4000);
 }
 
 // Thumbnail image controls
@@ -14,15 +22,10 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
 }
